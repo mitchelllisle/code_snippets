40c842ea84b4b3c87550d1902183f1e29ad5eabd
Mitchell <mitchell.lisle@bigdatr.com>
2017-05-13 16:21:33 +1000

    Update apiconnection.r

--- api/apiconnection.r
+++ api/apiconnection.r
@@ -1,17 +1,14 @@
 library(jsonlite)
 library(httr)
 
-# Enter any authentication details here
-userid <- ""
-secret <- ""
-url <- ""
+# Initialise data.frame()
+users <- data.frame()
 
 # Saves multiple pages of content to a data frame
 for (i in 1:1) {
-  req <- GET(paste0(url, i), authenticate(userid, secret, type = "basic"))
+  req <- GET(paste0(URL, i), authenticate(USERID, SECRET, type = "basic"))
   req_df <- data.frame(fromJSON(content(req, type = "text", encoding = "UTF-8"), flatten = TRUE))
-  # Initialise data.frame()
-  users <- data.frame()
+  
   # Here you can select only the columns you need from JSON to the Data Frame
   users <- rbind(artist_data, select(req_df, topartists.artist.name, topartists.artist.playcount, topartists.artist.mbid))
   message("Start: Page ", i)
