184b859ba2c47178cb10f79426e6077528860d1e
Mitchell <mitchell.lisle@bigdatr.com>
2017-05-13 16:03:01 +1000

    Update apiconnection.r

--- api/apiconnection.r
+++ api/apiconnection.r
@@ -10,7 +10,8 @@ url <- ""
 for (i in 1:1) {
   req <- GET(paste0(url, i), authenticate(userid, secret, type = "basic"))
   req_df <- data.frame(fromJSON(content(req, type = "text", encoding = "UTF-8"), flatten = TRUE))
-  users <- paste(req_df$users.email, req_df$users.name, req_df$users.custom_attributes.Company, req_df$users.created_at, req_df$users.signed_up, req_df$users.session_count, req_df$users.custom_attributes.Parent.Company, req_df$users.custom_attributes.Company.Type, req_df$users.custom_attributes.BigDatr...Industry, sep = ",")
+  # Here you can select only the columns you need from JSON to the Data Frame
+  users <- paste(req_df$COLUMNNAME, sep = ",")
   message("Start: Page ", i)
   print(users)
   message("End: Page ", i)
