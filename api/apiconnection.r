8daf79cb0e546d43b32f40845365f9c57dac27e2
Mitchell <mitchell.lisle@bigdatr.com>
2017-05-13 16:14:57 +1000

    Update apiconnection.r

--- api/apiconnection.r
+++ api/apiconnection.r
@@ -10,8 +10,10 @@ url <- ""
 for (i in 1:1) {
   req <- GET(paste0(url, i), authenticate(userid, secret, type = "basic"))
   req_df <- data.frame(fromJSON(content(req, type = "text", encoding = "UTF-8"), flatten = TRUE))
+  # Initialise data.frame()
+  users <- data.frame()
   # Here you can select only the columns you need from JSON to the Data Frame
-  users <- paste(req_df$COLUMNNAME, sep = ",")
+  users <- rbind(artist_data, select(req_df, topartists.artist.name, topartists.artist.playcount, topartists.artist.mbid))
   message("Start: Page ", i)
   print(users)
   message("End: Page ", i)
