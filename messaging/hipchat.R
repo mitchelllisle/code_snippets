cbed6da0964e3608cea904daf9dec6021c361a5c
Mitchell <mitchell.lisle@bigdatr.com>
2017-05-15 21:26:08 +1000

    Create hipchat.R

---
+++ messaging/hipchat.R
@@ -0,0 +1,7 @@
+# source: https://github.com/robertzk/hipchat
+
+Endpoints
+
+hipchat_send('room', 'AppSpeak', 'notification',
+             color = 'yellow', message = paste('Do Stirlitz Radio Names match Mongo?', answer[1]),
+             notify = TRUE, message_format = 'html')
