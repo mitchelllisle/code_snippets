0a6d208a5a3e0c16560b703d9a4ef3ebb58d6f85
Mitchell <mitchell.lisle@bigdatr.com>
2017-05-15 21:28:09 +1000

    Update hipchat.R

--- messaging/hipchat.R
+++ messaging/hipchat.R
@@ -1,7 +1,21 @@
 # source: https://github.com/robertzk/hipchat
 
-Endpoints
+# Endpoints
+
+# hipchat('Room name or email', "I'm in a hipchat!")
+# hipchat_topic('Room name', "This is the new topic")
+# hipchat_create_room('Room name')
+# hipchat_delete_room('Room name') # Will prompt a confirmation
+# hipchat_history('Room name') # A data.frame with messages
+# hipchat_rooms() # Fetch all available hipchat rooms
+# hichat_create_user(...)
+# hipchat_update_user(...)
+# hipchat_delete_user('terrible@employee.com')
+# hipchat_list_users()
+
+# You will need to have a HIPCHAT_API_TOKEN in your environment variables,
+# or set a options(hipchat.api_token = 'your_api_token') in your ~/.Rprofile.
 
 hipchat_send('room', 'AppSpeak', 'notification',
-             color = 'yellow', message = paste('Do Stirlitz Radio Names match Mongo?', answer[1]),
+             color = 'yellow', message = paste('This is a message with a variable', answer[1]),
              notify = TRUE, message_format = 'html')
