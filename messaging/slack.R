ea609064a1a8b0322a94a9aaa0a31a64bcc10d09
Mitchell <mitchell.lisle@bigdatr.com>
2017-05-15 21:33:16 +1000

    Update slack.R

--- messaging/slack.R
+++ messaging/slack.R
@@ -1,1 +1,12 @@
 # https://github.com/hrbrmstr/slackr
+# https://cran.r-project.org/web/packages/slackr/slackr.pdf
+
+# SlackR setup needs to be done first specifying this information. NOTE: You need to setup an API token first.
+# slackr_setup(channel = "#general", username = "slackr", icon_emoji = "",
+# incoming_webhook_url = "", api_token = "", config_file = "~/.slackr",
+# echo = FALSE)
+
+# Call environment variables needed
+  slackr_setup()
+# Send message using Slack Bot. Bot needs to be setup first. 
+  slackr_bot("This is a test message")
